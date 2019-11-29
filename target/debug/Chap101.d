db0c2be56a3073efc0c619405d14a5904192b95b
Your Name <Your Email>
2019-11-29 15:08:28 +0000

    first commit

---
+++ target/debug/Chap101.d
@@ -0,0 +1,1 @@
+/home/nida/Chap101/Chap101/target/debug/Chap101: /home/nida/Chap101/Chap101/src/main.rs
---
+++ target/debug/deps/Chap101-b8e1a2e8e4267fb2.d
@@ -0,0 +1,5 @@
+/home/nida/Chap101/Chap101/target/debug/deps/Chap101-b8e1a2e8e4267fb2: src/main.rs
+
+/home/nida/Chap101/Chap101/target/debug/deps/Chap101-b8e1a2e8e4267fb2.d: src/main.rs
+
+src/main.rs:
